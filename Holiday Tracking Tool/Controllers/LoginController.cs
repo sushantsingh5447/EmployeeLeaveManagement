d4a1ae707f22e4f2c5024d38dc07029564fb2346
sushantsingh <sushant.singh8283@outlook.com>
2017-06-07 16:10:34 +0530

    comitting code changes for the first time

---
+++ Holiday Tracking Tool/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Holiday_Tracking_Tool.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Authentication()
+        {
+            // console.writeine
+            return View("Authentication");
+        }
+    }
+}
---
+++ Holiday Tracking Tool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Holiday_Tracking_Tool.MvcApplication" Language="C#" %>
