17e21d82ecaa2e3d4580366cbb343d668ef5ea14
sushantsingh <sushant.singh8283@outlook.com>
2017-06-07 20:32:04 +0530

    creation of login model, controller  views js file css file

--- Holiday Tracking Tool/Controllers/LoginController.cs
+++ Holiday Tracking Tool/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Holiday_Tracking_Tool.Models;
 
 namespace Holiday_Tracking_Tool.Controllers
 {
@@ -13,6 +14,16 @@ namespace Holiday_Tracking_Tool.Controllers
         {
             // console.writeine
             return View("Authentication");
+        }
+        [HttpPost]
+        public ActionResult Validation(Login loginModel)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO: SubscribeUser(model.Email);
+            }
+            return View("");
+           // return View("Index", model);
         }
     }
 }
